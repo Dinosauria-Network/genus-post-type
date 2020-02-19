f5099e59c685a021813dadf208767c776ee6aa20
Alexander Bell <contact@alexanderbell.info>
2020-02-18 23:44:55 -0800

    Create taxon-post-type-template.php

---
+++ inc/taxon-post-type-template.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Add "Taxon" template to page attirbute template section.
+ */
+function genus_post_type_add_template_to_select( $post_templates, $wp_theme, $post, $post_type ) {
+
+    // Add custom template named template-taxon.php to select dropdown 
+    $post_templates['template-taxon.php'] = __('Taxon');
+
+    return $post_templates;
+}
+
+add_filter( 'theme_page_templates', 'genus_post_type_add_template_to_select', 10, 4 );
+
+
+/**
+ * Check if current page has our custom template. Try to load
+ * template from theme directory and if not exist load it 
+ * from root plugin directory.
+ */
+function genus_post_type_load_plugin_template( $template ) {
+
+    if(  get_page_template_slug() === 'template-taxon.php' ) {
+
+        if ( $theme_file = locate_template( array( 'template-taxon.php' ) ) ) {
+            $template = $theme_file;
+        } else {
+            $template = plugin_dir_path( __FILE__ ) . 'template-taxon.php';
+        }
+    }
+
+    if ($template == '') {
+        throw new \Exception('No template found');
+    }
+
+    return $template;
+}
+
+add_filter( 'template_include', 'genus_post_type_load_plugin_template' );
